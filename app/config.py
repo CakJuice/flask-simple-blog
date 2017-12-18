8603b5c5b6a50fa14dcab40fddf46fd2b9fc9d47
Hendra Juice <hd.brandoz@gmail.com>
2017-12-18 15:45:53 +0700

    bug fix

--- app/config.py
+++ app/config.py
@@ -1,7 +1,10 @@
+import os
+
 class Configuration(object):
 	APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
 	DEBUG = True
 	
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	"""
 	The SQLALCHEMY_DATABASE_URI comprises the following parts:
 	dialect+driver://username:password@host:port/database
