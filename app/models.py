501ff70b7c918554a26de4d923c520b132688941
Hendra Juice <hd.brandoz@gmail.com>
2017-12-19 12:59:12 +0700

    create models Tag

--- app/models.py
+++ app/models.py
@@ -23,4 +23,16 @@ class Entry(db.Model):
 			self.slug = slugify(self.title)
 
 	def __repr__(self):
-		return '<Entry: {}>'.format(self.title)
+		return '<Entry: {0}>'.format(self.title)
+
+class Tag(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64))
+	slug = db.Column(db.String(64), unique=True)
+
+	def __init__(self, *args, **kwargs):
+		super(Tag, self).__init__(*args, **kwargs)
+		self.slug = slugify(self.name)
+
+	def __repr__(self):
+		return '<Tag: {0}>'.format(self.name)
