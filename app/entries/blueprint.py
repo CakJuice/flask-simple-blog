f051c45b9caf2af4699e7192f8dfcb3e8e4449bf
Cak Juice <hd.brandoz@gmail.com>
2017-12-19 20:00:52 +0700

    update tag_detail

--- app/entries/blueprint.py
+++ app/entries/blueprint.py
@@ -15,9 +15,11 @@ def tag_index():
 
 @entries.route('/tags/<slug>/')
 def tag_detail(slug):
-	pass
+	tag = Tag.query.filter(Tag.slug == slug).first_or_404()
+	entries = tag.entries.order_by(Entry.created_timestamp.desc())
+	return object_list('entries/tag_detail.html', entries, tag=tag)
 
 @entries.route('/<slug>/')
 def detail(slug):
-	entry = Entry.query.filter(Entry.slug==slug).first_or_404()
+	entry = Entry.query.filter(Entry.slug == slug).first_or_404()
 	return render_template('entries/detail.html', entry=entry)
