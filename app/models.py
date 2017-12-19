1f72a858a01cbd10e76d66ff28b16cd0e3715009
Hendra Juice <hd.brandoz@gmail.com>
2017-12-19 13:17:47 +0700

    add relation entry tag

--- app/models.py
+++ app/models.py
@@ -4,6 +4,11 @@ from app import db
 def slugify(s):
 	return re.sub('[^\w]+', '-', s).lower()
 
+entry_tags = db.Table('entry_tags',
+	db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
+	db.Column('entry_id', db.Integer, db.ForeignKey('entry.id'))
+)
+
 class Entry(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(100))
@@ -11,6 +16,8 @@ class Entry(db.Model):
 	body = db.Column(db.Text)
 	created_timestamp = db.Column(db.DateTime, default=datetime.datetime.now)
 	modified_timestamp = db.Column(db.DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
+
+	tags = db.relationship('Tag', secondary=entry_tags, backref=db.backref('entries', lazy='dynamic'))
 
 	def __init__(self, *args, **kwargs):
 		# call parent constructor
