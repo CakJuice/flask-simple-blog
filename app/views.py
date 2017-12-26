4a4ea792518015569a2fd84219cce50e9123e033
Cak Juice <hd.brandoz@gmail.com>
2017-12-26 10:28:29 +0700

    check login status before login and logout

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, flash, redirect, url_for
+from flask import render_template, request, flash, redirect, url_for, g
 from flask_login import login_user, logout_user
 from app import app, login_manager
 from forms import LoginForm
@@ -9,18 +9,26 @@ def homepage():
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
-	if request.method == 'POST':
-		form = LoginForm(request.form)
-		if form.validate():
-			login_user(form.user, remember=form.remember_me.data)
-			flash("Successfully logged in as {0}.".format(form.user.email, 'success'))
-			return redirect(request.args.get('next') or url_for('homepage'))
+	if g.user.is_authenticated:
+		flash("You still logged in!", 'info')
+		return redirect(url_for('homepage'))
 	else:
-		form = LoginForm()
-	return render_template('login.html', form=form)
+		if request.method == 'POST':
+			form = LoginForm(request.form)
+			if form.validate():
+				login_user(form.user, remember=form.remember_me.data)
+				flash("Successfully logged in as {0}.".format(form.user.email, 'success'))
+				return redirect(request.args.get('next') or url_for('homepage'))
+		else:
+			form = LoginForm()
+		return render_template('login.html', form=form)
 
 @app.route('/logout/')
 def logout():
-	logout_user()
-	flash("You've have been logged out.", 'success')
-	return redirect(request.args.get('next') or url_for('homepage'))
+	if g.user.is_authenticated:
+		logout_user()
+		flash("You have been logged out.", 'success')
+		return redirect(request.args.get('next') or url_for('homepage'))
+	else:
+		flash("You're not logged in!", 'info')
+		return redirect(url_for('homepage'))
