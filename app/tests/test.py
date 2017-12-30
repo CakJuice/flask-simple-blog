d7dd437939bd76c18c776ec4e4bb34d2c8b5fd52
Hendra Juice <hd.brandoz@gmail.com>
2017-12-30 10:36:59 +0700

    test admin page

--- app/tests/test.py
+++ app/tests/test.py
@@ -17,6 +17,8 @@ class FlaskLoginMixin():
 		return self.app.get(self.LOGOUT_URL, follow_redirects=True)
 
 class AppTest(unittest.TestCase, FlaskLoginMixin):
+	ADMIN_URL = '/admin/'
+	
 	def setUp(self):
 		self.app = app.test_client()
 
@@ -39,6 +41,30 @@ class AppTest(unittest.TestCase, FlaskLoginMixin):
 		response_logout = self.logout()
 		self.assertEqual(response_logout.status_code, 200)
 		self.assertTrue("not logged in" in str(response_logout.data))
+
+	def test_admin_can_get_admin_page(self):
+		self.login("cakjuice@test.com", "cakjuice")
+		response = self.app.get(self.ADMIN_URL)
+		self.assertEqual(response.status_code, 200)
+		self.assertTrue("Hello" in str(response.data))
+
+	def test_non_logged_in_user_can_get_to_admin_page(self):
+		response = self.app.get(self.ADMIN_URL)
+		self.assertEqual(response.status_code, 302)
+		self.assertTrue("redirected" in str(response.data))
+
+	def test_non_admin_user_cannot_get_to_admin_page(self):
+		self.login("test@test.com", "test")
+		response = self.app.get(self.ADMIN_URL)
+		self.assertEqual(response.status_code, 302)
+		self.assertTrue("redirected" in str(response.data))
+
+	def test_logging_out_prevents_access_to_admin_page(self):
+		self.login("cakjuice@test.com", "cakjuice")
+		self.logout()
+		response = self.app.get(self.ADMIN_URL)
+		self.assertEqual(response.status_code, 302)
+		self.assertTrue("redirected" in str(response.data))
 
 if __name__ == '__main__':
 	unittest.main()
